ee626f65bbbe87cb176d248e40c7e2e86573f4e2
chungphb <phamhuubaochung@gmail.com>
2025-04-14 06:44:40 +0700

    Function: Overloading

--- contracts/Function.sol
+++ contracts/Function.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.26;
 
 contract Function {
+    // Test function overloading.
+    function sum(uint a, uint b) public pure returns (uint) {
+        return a + b;
+    }
+
+    function sum(uint a, uint b, uint c) public pure returns (uint) {
+        return a + b + c;
+    }
+
     // Test fallback functions.
     function testFallback(Receiver receiver) public {
         (bool success,) = address(receiver).call(abi.encodeWithSignature("nonExistingFunction()"));
